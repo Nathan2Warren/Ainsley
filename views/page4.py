9e8395ce8c0385e4cb0fcf932f5475141bb709f1
Nathan2Warren <nathan.warren222@gmail.com>
2022-03-22 21:19:08 -0400

    placeholder code

---
+++ views/page4.py
@@ -0,0 +1,9 @@
+import dash
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+
+from app import app
+
+layout = dbc.Container(dbc.Row([dbc.Col([])]))
